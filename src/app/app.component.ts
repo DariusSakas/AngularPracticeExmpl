0bae8867297bda39e60b74a54cdd8ebed5f1d7ee
Darius <sakas.darius@gmail.com>
2021-05-12 21:30:49 +0300

    Binding  exmpl added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,13 @@ export class AppComponent {
   number = 5;
   arr = [1, 2, 3, 10];
   obj = { a: 1, b: 3 };
+  img = 'https://d29fhpw069ctt2.cloudfront.net/icon/image/38737/preview.svg';
+
+  constructor() {
+    setTimeout(() => {
+      console.log('Time out is over');
+      this.img =
+        'https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_angular_icon_130754.png';
+    }, 5000);
+  }
 }
