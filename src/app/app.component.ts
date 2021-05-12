e7e99a4062698562307d56cdbd8221b8d190c07a
Darius <sakas.darius@gmail.com>
2021-05-12 20:47:45 +0300

    Added new exmpl with Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'AngularPracticeExmpl';
+  title = 'Dynamic title';
+  number = 5;
+  arr = [1, 2, 3, 10];
+  obj = { a: 1, b: 3 };
 }
